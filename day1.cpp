a8f3682f0f592b3dcf7522957a00b8afbde71d71
Balendra Singh <74133152+Balendrasingh2210@users.noreply.github.com>
2023-05-02 14:17:57 +0530

    day1 notes

---
+++ day1.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cin doesn't read space tab and enter
+    // to read these characters we use cin.get() which reads character
+    // a = cin.get();
+    // cout << "the value of a is:" << a << endl;
+    // int a, b;
+    // cout << "Enter the value of a " << endl;
+    // cin >> a;
+    // cout << "Enter the value of b" << endl;
+    // cin >> b;
+    // if (a > b)
+    // {
+    //     cout << "a is greater than b" << endl;
+    // }
+    // else
+    // {
+    //     cout << "b is greater than a" << endl;
+    // }
+    // int a = 2;
+    // int b = a + 1;
+    // if ((a = 3) == b)
+    // {
+    //     cout << "first if" << a;
+    // }
+    // else
+    // {
+    //     cout << a + 1;
+    // }
+    // char ch;
+    // cin >> ch;
+    // if (ch >= 'a' && ch <= 'z')
+    // {
+    //     cout << "This is lowercase";
+    // }
+    // else if (ch >= 'A' && ch <= 'Z')
+    // {
+    //     cout << "this is uppercase";
+    // }
+    // else if (ch >= '0' && ch <= '9')
+    // {
+    //     cout << "this is numeric";
+    // }
+    // int n;
+    // cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     cout << i << " ";
+    //     i++;
+    // }
+    // ques - find the sum of even numbers
+    // int n;
+    // cin >> n;
+    // int i = 2;
+    // int sum = 0;
+    // while (i <= n)
+    // {
+    //     sum = sum + i;
+    //     i = i + 2;
+    // }
+    // cout << sum;
+    // ques - given number is prime or not
+    // int n;
+    // cin >> n;
+    // int i = 2;
+    // int flag = 0;
+    // while (i < n)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         flag = 1;
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         i++;
+    //     }
+    // }
+    // if (flag == 0)
+    // {
+    //     cout << "number is prime";
+    // }
+    // else
+    // {
+    //     cout << "number is not prime";
+    // }
+
+    // question pattern of star n*n
+
+    // int n;
+    // cin >> n;
+
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << "*";
+    //         j++;
+    //     }
+    //     i++;
+    //     cout << endl;
+    // }
+    int n;
+    cin >> n;
+
+    int i = 1;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << i;
+            j++;
+        }
+        i++;
+        cout << "\n";
+    }
+}
