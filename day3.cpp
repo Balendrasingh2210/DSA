0450a43a4a924bc58ba36191e65e4ce12204fad9
Balendra Singh <74133152+Balendrasingh2210@users.noreply.github.com>
2023-05-04 22:52:04 +0530

    day3

---
+++ day3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 4;
+    // int b = 6;
+    // cout << " a&b " << (a & b) << endl;
+    // cout << " a|b " << (a | b) << endl;
+    // cout << " ~a " << (~a) << endl;
+    // cout << " a^b " << (a ^ b) << endl;
+    // //  a&b 4
+    // //  a|b 6
+    // //  ~a -5
+    // //  a^b 2
+    // cout << (17 >> 1) << endl;
+    // cout << (17 >> 2) << endl;
+    // cout << (19 << 1) << endl;
+    // cout << (19 << 2) << endl;
+
+    // sum of n natural numbers
+    // int n;
+    // cin >> n;
+
+    // // int sum = 0;
+    // // for (int i = 1; i <= n; i++)
+    // // {
+    // //     sum += i;
+    // // }
+    // // cout << sum << endl;
+
+    // // fibonacci series
+    // int a = 0, b = 1;
+    // cout << a << " " << b << " ";
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     int next = a + b;
+    //     cout << next << " ";
+    //     a = b;
+    //     b = next;
+    // }
+    // solved question number 1281 on leetcode
+    // return the difference of product of digits and sum of digits of a number
+    // solved question number 191 on leetcode
+    // return the set digits of a number
+    // int hammingWeight(uint32_t n)
+    // {
+    //     int count = 0;
+    //     while (n)
+    //     {
+    //         if (n & 1)
+    //         {
+    //             count++;
+    //         }
+    //         n = n >> 1;
+    //     }
+    //     return count;
+    // }
+
+    // decimal to binary
+}
